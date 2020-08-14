c24c20d104d43a03d075f45fe449569d7767c9fb
Luka-dot <58044174+Luka-dot@users.noreply.github.com>
2020-08-13 22:05:03 -0400

    data changes

--- data/dummy-data.js
+++ data/dummy-data.js
@@ -9,6 +9,7 @@ const PRODUCTS = [
     'A red t-shirt, perfect for days with non-red weather.',
     29.99
   ),
+  
   new Product(
     'p2',
     'u1',
@@ -17,6 +18,7 @@ const PRODUCTS = [
     'Fits your red shirt perfectly. To stand on. Not to wear it.',
     99.99
   ),
+
   new Product(
     'p3',
     'u2',
@@ -25,6 +27,7 @@ const PRODUCTS = [
     'Can also be used for tea!',
     8.99
   ),
+
   new Product(
     'p4',
     'u3',
@@ -33,6 +36,7 @@ const PRODUCTS = [
     "What the content is? Why would that matter? It's a limited edition!",
     15.99
   ),
+
   new Product(
     'p5',
     'u3',
@@ -41,6 +45,7 @@ const PRODUCTS = [
     'Awesome hardware, crappy keyboard and a hefty price. Buy now before a new one is released!',
     2299.99
   ),
+
   new Product(
     'p6',
     'u1',
