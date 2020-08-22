ab3ecd9000d5dadecd0a9403dd6acad06900eac1
Luka-dot <58044174+Luka-dot@users.noreply.github.com>
2020-08-22 17:16:28 -0400

    order action to save serverside

--- store/actions/orders.js
+++ store/actions/orders.js
@@ -2,31 +2,31 @@ export const ADD_ORDER = 'ADD_ORDER';
 
 export const addOrder = (cartItems, totalAmount) => {
   return async dispatch => {
-      const date = new Date();
-          // can execute any async code.
-          const response = await fetch('https://rn-store-9a607.firebaseio.com/orders/u1.json', 
-            {
-              method: 'POST',
-              headers: {
-                  'Content-Type': 'application/json'
-              },
-              body: JSON.stringify({
-                  cartItems,
-                  totalAmount,
-                  date: date.toISOString()
-              })
-          }
-        );
-  
-          if (!response.ok) {
-            throw new Error('Something went wrong!');
-          }
+    const date = new Date();
+    // can execute any async code.
+    const response = await fetch('https://rn-store-9a607.firebaseio.com/orders/u1.json',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          cartItems,
+          totalAmount,
+          date: date.toISOString()
+        })
+      }
+    );
 
-          const resData = await response.json();
+    if (!response.ok) {
+      throw new Error('Something went wrong!');
+    }
+
+    const resData = await response.json();
 
     dispatch({
-        type: ADD_ORDER,
-        orderData: { id: resData.name, items: cartItems, amount: totalAmount, date: date }
-      });
+      type: ADD_ORDER,
+      orderData: { id: resData.name, items: cartItems, amount: totalAmount, date: date }
+    });
   };
 };
