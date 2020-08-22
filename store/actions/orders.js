d7e84447218d3916a0d8d9ac0d91420621b369eb
Luka-dot <58044174+Luka-dot@users.noreply.github.com>
2020-08-22 17:38:59 -0400

    Ordrs action for fetching Orders

--- store/actions/orders.js
+++ store/actions/orders.js
@@ -1,4 +1,11 @@
 export const ADD_ORDER = 'ADD_ORDER';
+export const SET_ORDERS = 'SET_ORDERS';
+
+export const fetchOrders = () => {
+  return async dispatch => {
+    dispatch({type: SET_ORDERS, orders: []})
+  }
+}
 
 export const addOrder = (cartItems, totalAmount) => {
   return async dispatch => {
