76a9873ef2c52839c5b0c9c94d40cf0fe51174ea
Luka-dot <58044174+Luka-dot@users.noreply.github.com>
2020-08-23 15:10:50 -0400

    Auth screen input

--- screens/user/AuthScreen.js
+++ screens/user/AuthScreen.js
@@ -2,8 +2,33 @@ import React from 'react';
 import { ScrollView, View, KeyboardAvoidingView, StyleSheet } from 'react-native';
 
 import Input from '../../components/UI/Input';
+import Card from '../../components/UI/Card';
 
-const AuthScreen = props => {};
+const AuthScreen = props => {
+    return (
+        <KeyboardAvoidingView 
+            behavior='padding'
+            keyboardVerticalOffset={50} 
+            style={styles.screen}
+        >
+            <Card style={styles.authContainer} >
+                <ScrollView>
+                    <Input 
+                        id="email" 
+                        label="EMAIL"
+                        keyboardType="email-address"
+                        required
+                        email
+                        autoCapitalize="none"
+                        errorMessage="Please enter valid email address"
+                        onValueChange={() => {}}
+                        initialValue=""
+                    />
+                </ScrollView>
+            </Card>
+        </KeyboardAvoidingView>
+    )
+};
 
 const styles = StyleSheet.create({
 
