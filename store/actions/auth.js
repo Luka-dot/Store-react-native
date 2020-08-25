485a4a4f03842daaf41216ae5e18cab07fcb60c7
Luka-dot <58044174+Luka-dot@users.noreply.github.com>
2020-08-25 11:22:55 -0400

    Auth log in 400 error fixed

--- store/actions/auth.js
+++ store/actions/auth.js
@@ -1,28 +1,32 @@
 export const SIGNUP = 'SIGNUP';
 
 export const signup = (email, password) => {
-    return async dispatch => {
-        const response = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDkm1Y5fG-StGlT2sIotG6bPRsKXW1l1wE',
-        {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                email: email,
-                password: password,
-                returnSecureToken: true
-            })
-        }
-        );
+  return async dispatch => {
+    const response = await fetch(
+      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDkm1Y5fG-StGlT2sIotG6bPRsKXW1l1wE',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          email: email,
+          password: password,
+          returnSecureToken: true
+        })
+      }
+    );
+      console.log(response)
+    if (!response.ok) {
+      throw new Error('Something went wrong!');
+    }
 
-        if (!response.ok) {
-            throw new Error('Something went wrong!');
-        }
-
-        const resData = await response.json();
-        console.log(resData);
-
-        dispatch({ type: SIGNUP })
-    };
+    const resData = await response.json();
+    console.log(resData);
+    dispatch({ type: SIGNUP });
+  };
 };
+
+
+
+
