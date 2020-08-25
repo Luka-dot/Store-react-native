b3d5426fedfe97ee77407df6a123e75e9b31686b
Luka-dot <58044174+Luka-dot@users.noreply.github.com>
2020-08-24 20:47:06 -0400

    Auth action set up fetch

--- store/actions/auth.js
+++ store/actions/auth.js
@@ -2,6 +2,18 @@ export const SIGNUP = 'SIGNUP';
 
 export const signup = (email, password) => {
     return async dispatch => {
+        fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDkm1Y5fG-StGlT2sIotG6bPRsKXW1l1wE',
+        {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                
+            })
+        }
+        );
+
         dispatch({ type: SIGNUP })
     };
 };
