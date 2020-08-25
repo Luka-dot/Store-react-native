2f48901eb3f10e3221b03de6ca6dea113a3c3737
Luka-dot <58044174+Luka-dot@users.noreply.github.com>
2020-08-24 20:50:02 -0400

    Auth action set up fetch for dispatch

--- store/actions/auth.js
+++ store/actions/auth.js
@@ -2,17 +2,26 @@ export const SIGNUP = 'SIGNUP';
 
 export const signup = (email, password) => {
     return async dispatch => {
-        fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDkm1Y5fG-StGlT2sIotG6bPRsKXW1l1wE',
+        const response = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDkm1Y5fG-StGlT2sIotG6bPRsKXW1l1wE',
         {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({
-                
+                email: email,
+                password: password,
+                returnSecureToken: true
             })
         }
         );
+
+        if (!response.ok) {
+            throw new Error('Something went wrong!');
+        }
+
+        const resData = await response.json();
+        console.log(resData);
 
         dispatch({ type: SIGNUP })
     };
