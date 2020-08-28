61f216b645ac2619b72842e0e76cfa33127a30ba
Luka-dot <79lukask@gmail.com>
2020-08-27 21:13:37 -0400

    startup screen auth finished

--- screens/StartupScreen.js
+++ screens/StartupScreen.js
@@ -1,8 +1,12 @@
 import React, { useEffect } from 'react';
 import { View,ActivityIndicator, StyleSheet, AsyncStorage } from 'react-native';
+import { useDispatch } from 'react-redux';
+
 import Colors from '../constants/Colors';
+import * as authActions from '../store/actions/auth';
 
 const StartupScreen = props => {
+    const dispatch = useDispatch();
     useEffect(() => {
         const tryLogin = async () => {
             const userData = await AsyncStorage.getItem('userData');
@@ -20,10 +24,11 @@ const StartupScreen = props => {
             }
 
             props.navigation.navigate('Shop');
+            dispatch(authActions.authenticate(userId, token));
         };
 
         tryLogin();
-    }, []);
+    }, [dispatch]);
 
     return (
         <View style={styles.screen} >
