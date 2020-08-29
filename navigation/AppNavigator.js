db8206e1eff3002c55218aa38caf313b35d98efc
Luka-dot <79lukask@gmail.com>
2020-08-29 13:00:40 -0400

    NavContainer chabged to nav5

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack'
 
 import ShopNavigator from './ShopNavigator';
+import ProductsOverviewScreen from '../screens/shop/ProductsOverviewScreen';
+
+const MyStack = createStackNavigator();
 
 const AppNavigator = props => {
     
@@ -14,7 +18,12 @@ const AppNavigator = props => {
     //     }
     // }, [isAuth])
 
-    return ;
+    return ( <NavigationContainer>
+                <MyStack.Navigator>
+                    <MyStack.Screen name="ProductsOverview" component={ProductsOverviewScreen} />
+                </MyStack.Navigator>
+            </NavigationContainer>
+    );
 };
 
 export default AppNavigator;
