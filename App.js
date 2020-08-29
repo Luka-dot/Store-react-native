17f577bf2f8ecdee7a9f24e1b285bb0a48ff5d01
Luka-dot <79lukask@gmail.com>
2020-08-29 12:51:26 -0400

    NavContainer chabged to AppNavigator

--- App.js
+++ App.js
@@ -9,7 +9,7 @@ import productsReducer from './store/reducers/products';
 import cartReducer from './store/reducers/cart';
 import ordersReducer from './store/reducers/orders';
 import authReducer from './store/reducers/auth';
-import AppNavigator from './navigation/NavigationContainer';
+import AppNavigator from './navigation/AppNavigator';
 
 const rootReducer = combineReducers({
   products: productsReducer,
