019bfce2abec50cfdb176a70958eb9db7ba61c51
Luka-dot <79lukask@gmail.com>
2020-09-08 23:40:23 -0400

    small change to start screen

--- screens/shop/CartScreen.js
+++ screens/shop/CartScreen.js
@@ -1,12 +1,5 @@
 import React, { useState } from 'react';
-import {
-  View,
-  Text,
-  FlatList,
-  Button,
-  StyleSheet,
-  ActivityIndicator
-} from 'react-native';
+import { View, Text, FlatList, Button, StyleSheet, ActivityIndicator } from 'react-native';
 import { useSelector, useDispatch } from 'react-redux';
 
 import Colors from '../../constants/Colors';
